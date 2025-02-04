6005bcd327f47f9d420e60ebf5de295d41cc1bda
Nethma de silva <nethmavilhan@gmail.com>
2025-02-04 15:32:46 +0530

    Add all Files

--- resources/views/teachers/show.blade.php
+++ resources/views/teachers/show.blade.php
@@ -2,12 +2,12 @@
 @section('content')
  
 <div class="card">
-  <div class="card-header">Student Page</div>
+  <div class="card-header">Teachers Page</div>
   <div class="card-body">
     <div class="card-body">
-      <h5 class="card-title">Name: {{ $student->name }}</h5>
-      <p class="card-text">Address: {{ $student->address }}</p>
-      <p class="card-text">Mobile: {{ $student->mobile }}</p>
+      <h5 class="card-title">Name: {{ $teacher->name }}</h5>
+      <p class="card-text">Address: {{ $teacher->address }}</p>
+      <p class="card-text">Mobile: {{ $teacher->mobile }}</p>
     </div>
   </div>
 </div>
