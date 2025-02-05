2343326351d92108888a4994935c98401888277d
Nethma de silva <nethmavilhan@gmail.com>
2025-02-05 16:01:14 +0530

    Add all changes

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\TeacherController;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\BatchController;
 
 Route::get('/', function () {
     return view('layout');
 });
 Route::resource('/students',StudentController::class);
 Route::resource('/teachers',TeacherController::class);
+Route::resource('/courses',CourseController::class);
+Route::resource('/batches',BatchController::class);
 
 Route::middleware([
     'auth:sanctum',
